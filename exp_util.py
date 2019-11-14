f09fd0f978e493163388d449eefc4de801a74b8e
cheechingong <57111360+cheechingong@users.noreply.github.com>
2019-11-13 20:00:06 -0800

    Add files via upload

--- exp_util.py
+++ exp_util.py
@@ -8,3 +8,12 @@ def val_bin_accuracy(reg, xval, yval):
     bin_pred = classify_output(reg.predict(xval))
     bin_label = classify_output(yval)
     return np.mean(bin_pred == bin_label)
+
+def classify_output_two_classes(x):
+    f = lambda x: 0 if x < 30 else 1
+    return np.array(list(map(f, x)))
+
+def val_bin_accuracy_two_classes(reg, xval, yval):
+    bin_pred = classify_output_two_classes(reg.predict(xval))
+    bin_label = classify_output_two_classes(yval)
+    return np.mean(bin_pred == bin_label)
